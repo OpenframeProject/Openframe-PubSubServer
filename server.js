c78cafdce9fbc89811dc3d769a261ec12f90f75e
Jonathan Wohl <jon@jonwohl.com>
2015-12-13 10:02:27 -0500

    initial local commit

---
+++ server.js
@@ -0,0 +1,21 @@
+var http = require('http'),
+    faye = require('faye');
+
+var server = http.createServer(),
+    bayeux = new faye.NodeAdapter({mount: '/faye', timeout: 45});
+
+/**
+ * Handle new client connection
+ * @param  {[type]} clientId) {}          [description]
+ * @return {[type]}           [description]
+ */
+bayeux.on('handshake', function(clientId) {
+    console.log('new bayeux connection: ', clientId);
+});
+
+bayeux.attach(server);
+server.listen(8000);
+
+bayeux.getClient().subscribe('/frame/updated/*', function(data) {
+    console.log(data);
+});

d62e787a3ebbff5319b15a9a9108791a032f6ddd
Jonathan Wohl <jon@jonwohl.com>
2016-02-14 15:47:11 -0500

    Make module runnable programmatically

--- README.md
+++ README.md
@@ -5,6 +5,36 @@ This repo represents the **Global Event Bus (pubsub)** in the following diagram.
 
 ![alt tag](https://raw.github.com/OpenframeProject/Openframe-API/master/docs/img/API Diagram.jpg)
 
+## Overview
+
+This is a very basic implementation using [Faye](faye.jcoglan.com). It acts purely as an event bus, with no persistence or message queue.
+
+The pubsub server keeps track of which frames are connected in order to send out a 'disconnected' event when a frame's connection is closed.
+
+As it is a Faye server, clients can connect using the Faye client.
+
+## Usage
+
+To start up the server from the command line:
+
+```bash
+$ npm start
+```
+
+Or programmatically:
+
+```javascript
+
+var pubsubServer = require('openframe-pubsubserver');
+
+// defaults to port 8889
+pubsubServer.start();
+
+// alternatively, pass a port into the start method:
+// pubsubServer.start(1234);
+
+```
+
 ## Built-in System Events (WIP)
 
 #### /frame/connected/{frame_id}
@@ -12,6 +42,9 @@ This repo represents the **Global Event Bus (pubsub)** in the following diagram.
 #### /frame/disconnected/{frame_id}
 
 #### /frame/updated/{frame_id}
+
+
+## Proposed (not implemented)
 
 #### /user/loggedin/{user_name}
 
--- index.js
+++ index.js
@@ -1,49 +1,62 @@
-var http = require('http'),
-    faye = require('faye');
-
-var server = http.createServer(),
-    bayeux = new faye.NodeAdapter({mount: '/faye', timeout: 5}),
-
-    // hash of connected frames
-    frames = {};
-
 /**
- * Handle new client connection
- * @param  {[type]} clientId) {}          [description]
- * @return {[type]}           [description]
+ * The pubsub server is very basic at present... it just spins up
+ * an instance of a Faye server, allowing clients to communicate via bayeux
  */
-bayeux.on('handshake', function(clientId) {
-    console.log('new bayeux connection: ', clientId);
-});
 
-// When a client disconnects, see if it's in the hash of connected frames.
-// If so, publish a disconnect event to any subscribed clients (e.g. the API server)
-bayeux.on('disconnect', function(clientId) {
-    console.log('bayeux connection closed: ', clientId);
-    if (frames[clientId]) {
-        bayeux.getClient().publish('/frame/disconnected', frame_id);
-    }
-});
+var http = require('http'),
+    faye = require('faye'),
+    debug = require('debug')('pubsub');
 
-// monitor all publish events. if we get an event on /frame/connected,
-// add it to the list of connected frames
-bayeux.on('publish', function(clientId, channel, data) {
-    console.log('publish event:', clientId, channel, data);
-    if (channel === '/frame/connected') {
-        console.log('heard /frame/connected event');
-        frames[clientId] = data;
-    }
-});
+    // Exported object
+    pubsub = module.exports = {};
 
-bayeux.attach(server);
-server.listen(8889);
+pubsub.connectedFrames = {};
 
-// Just some logging...
-// TODO: log to file.
-bayeux.getClient().subscribe('/frame/connected', function(data) {
-    console.log('/frame/connected', data);
-});
+pubsub.start = function(port) {
+    port = port || '8889';
 
-bayeux.getClient().subscribe('/frame/disconnected', function(data) {
-    console.log('/frame/disconnected', data);
-});
+    var server = http.createServer(),
+        bayeux = new faye.NodeAdapter({mount: '/faye', timeout: 5});
+
+    /**
+     * Handle new client connection
+     * @param  {[type]} clientId) {}          [description]
+     * @return {[type]}           [description]
+     */
+    bayeux.on('handshake', function(clientId) {
+        debug('new bayeux connection: ', clientId);
+    });
+
+    // When a client disconnects, see if it's in the hash of connected frames.
+    // If so, publish a disconnect event to any subscribed pubsub clients (e.g. the API server)
+    bayeux.on('disconnect', function(clientId) {
+        debug('bayeux connection closed: ', clientId);
+        if (pubsub.connectedFrames[clientId]) {
+            bayeux.getClient().publish('/frame/disconnected', frame_id);
+        }
+    });
+
+    // monitor all publish events. if we get an event on /frame/connected,
+    // add it to the list of connected frames
+    bayeux.on('publish', function(clientId, channel, data) {
+        debug('publish event:', clientId, channel, data);
+        if (channel === '/frame/connected') {
+            debug('heard /frame/connected event');
+            pubsub.connectedFrames[clientId] = data;
+        }
+    });
+
+    bayeux.attach(server);
+    server.listen(port);
+
+    // Just some logging...
+    // TODO: log to file.
+    bayeux.getClient().subscribe('/frame/connected', function(data) {
+        debug('/frame/connected', data);
+    });
+
+    bayeux.getClient().subscribe('/frame/disconnected', function(data) {
+        debug('/frame/disconnected', data);
+    });
+
+}
